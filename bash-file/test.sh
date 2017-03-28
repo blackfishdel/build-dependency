a35ef0ac59f85a3d3837fc48a1c5c0ee2b9b4aaf
delxie <delxie@del.xie>
2017-03-28 15:04:50 +0800

    repair bug,pro type is no only

--- bash-file/test.sh
+++ bash-file/test.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -e
-json_description=$(cat "/home/delxie/Documents/git-repository/bssp/description.json")
 
-#循环遍历
-jq_dependencies=($(echo ${json_description} | jq "recurse(.dependencies[]) | .dependencies | tostring"))
-for ((i=${#jq_dependencies[@]};i>0;i--));do
-	jq_i=`expr i-1`
-	jq_dependency=$(echo ${jq_dependencies[${jq_i}]} | jq "fromjson")
-	jq_dependency_length=$(echo ${jq_dependency} | jq ".[] | length")
-	echo ${jq_dependency}
-done
+#修改父pom版本号
+
+#修改父pom外部依赖版本号
